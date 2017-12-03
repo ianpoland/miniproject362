b3e258c199550a729457c81e19f006dff4b8ec7b
Sam <jenkin49@purdue.edu>
2017-12-03 14:43:37 -0500

    Sam's changes, added file functions for SCI capabilities

---
+++ finger.h
@@ -0,0 +1,203 @@
+//
+//  serial_com.h
+//  Fingerprint Library
+//
+//  Created by Sam Jenkins on 12/1/17.
+//
+//
+
+#ifndef serial_com_h
+#define serial_com_h
+
+// command code definitions
+#define NotSet               0x00		// Default value for enum. Scanner will return error if sent this.
+#define Open				 0x01		// Open Initialization
+#define Close				 0x02		// Close Termination
+#define ChangeEBaudRate		 0x04		// ChangeBaudrate Change UART baud rate
+#define CmosLed				 0x12		// CmosLed Control CMOS LED
+#define GetEnrollCount		 0x20		// Get enrolled fingerprint count
+#define CheckEnrolled		 0x21		// Check whether the specified ID is already enrolled
+#define EnrollStart			 0x22		// Start an enrollment
+#define Enroll1				 0x23		// Make 1st template for an enrollment
+#define Enroll2				 0x24		// Make 2nd template for an enrollment
+#define Enroll3				 0x25		// Make 3rd template for an enrollment, merge three templates into one template, save merged template to the database
+#define IsPressFinger		 0x26		// Check if a finger is placed on the sensor
+#define DeleteID			 0x40		// Delete the fingerprint with the specified ID
+#define DeleteAll			 0x41	// Delete all fingerprints from the database
+#define Verify1_1			 0x50		// Verification of the capture fingerprint image with the specified ID
+#define Identify1_N			 0x51		// Identification of the capture fingerprint image with the database
+#define VerifyTemplate1_1	 0x52		// Verification of a fingerprint template with the specified ID
+#define IdentifyTemplate1_N	 0x53	// Identification of a fingerprint template with the database
+#define CaptureFinger		 0x60		// Capture a fingerprint image(256x256) from the sensor
+#define MakeTemplate		 0x61		// Make template for transmission
+#define GetImage             0x62		// Download the captured fingerprint image(256x256)
+#define GetRawImage			 0x63		// Capture & Download raw fingerprint image(320x240)
+#define GetTemplate			 0x70		// Download the template of the specified ID
+#define SetTemplate			 0x71		// Upload the template of the specified ID
+#define GetDatabaseStart	 0x72		// Start database download, obsolete
+#define GetDatabaseEnd		 0x73		// End database download, obsolete
+#define Ack					 0x30		// Acknowledge.
+#define Nack				 0x31		// Non-acknowledge
+
+//
+#define COMMAND_START_CODE_1 0x55	// Static byte to mark the beginning of a command packet	-	never changes
+#define COMMAND_START_CODE_2 0xAA	// Static byte to mark the beginning of a command packet	-	never changes
+#define COMMAND_DEVICE_ID_1  0x01	// Device ID Byte 1 (lesser byte)							-	theoretically never changes
+#define COMMAND_DEVICE_ID_2  0x00
+
+// error code definitions
+#define NO_ERROR					 0x0000	// Default value. no error
+#define NACK_TIMEOUT				 0x1001	// Obsolete, capture timeout
+#define NACK_INVALID_BAUDRATE		 0x1002	// Obsolete, Invalid serial baud rate
+#define NACK_INVALID_POS			 0x1003	// The specified ID is not between 0~199
+#define NACK_IS_NOT_USED			 0x1004	// The specified ID is not used
+#define NACK_IS_ALREADY_USED	     0x1005	// The specified ID is already used
+#define NACK_COMM_ERR				 0x1006	// Communication Error
+#define NACK_VERIFY_FAILED			 0x1007	// 1:1 Verification Failure
+#define NACK_IDENTIFY_FAILED		 0x1008	// 1:N Identification Failure
+#define NACK_DB_IS_FULL				 0x1009	// The database is full
+#define NACK_DB_IS_EMPTY			 0x100A	// The database is empty
+#define NACK_TURN_ERR			     0x100B	// Obsolete, Invalid order of the enrollment (The order was not as: EnrollStart -> Enroll1 -> Enroll2 -> Enroll3)
+#define NACK_BAD_FINGER				 0x100C	// Too bad fingerprint
+#define NACK_ENROLL_FAILED			 0x100D	// Enrollment Failure
+#define NACK_IS_NOT_SUPPORTED		 0x100E	// The specified command is not supported
+#define NACK_DEV_ERR			     0x100F	// Device Error, especially if Crypto-Chip is trouble
+#define NACK_CAPTURE_CANCELED		 0x1010	// Obsolete, The capturing is canceled
+#define NACK_INVALID_PARAM			 0x1011	// Invalid parameter
+#define NACK_FINGER_IS_NOT_PRESSED	 0x1012	// Finger is not pressed
+#define INVALID						 0XFFFF	// Used when parsing fails
+
+
+
+typedef unsigned char byte;
+typedef unsigned int word;
+
+
+
+struct command_packet {
+    byte Parameter[4]; // 4 bytes
+    byte command[2];  // 2 bytes
+
+};
+
+struct response_packet {
+    byte* RawBytes;
+    byte* ParameterBytes;
+    byte* ResponseBytes;
+
+};
+
+byte* GetPacketBytes();							// returns the bytes to be transmitted
+void ParameterFromInt(int i, byte* Parameter);
+
+word _CalculateChecksumOut(struct command_packet* pack);						// Checksum is calculated using byte addition
+byte GetHighByte(word w);
+byte GetLowByte(word w);
+
+
+//Initialises the device and gets ready for commands
+void open();
+
+// Does not actually do anything (according to the datasheet)
+// I implemented open, so had to do closed too... lol
+void close();
+
+// Turns on or off the LED backlight
+// LED must be on to see fingerprints
+// Parameter: true turns on the backlight, false turns it off
+// Returns: True if successful, false if not
+int SetLED(int on);
+
+// Changes the baud rate of the connection
+// Parameter: 9600 - 115200
+// Returns: True if success, false if invalid baud
+// NOTE: Untested (don't have a logic level changer and a voltage divider is too slow)
+int ChangeBaudRate(unsigned long baud);
+
+// Gets the number of enrolled fingerprints
+// Return: The total number of enrolled fingerprints
+int get_enroll_count();
+
+// checks to see if the ID number is in use or not
+// Parameter: 0-199
+// Return: True if the ID number is enrolled, false if not
+int check_enrolled(int id);
+
+// Starts the Enrollment Process
+// Parameter: 0-199
+// Return:
+//	0 - ACK
+//	1 - Database is full
+//	2 - Invalid Position
+//	3 - Position(ID) is already used
+int enroll_start(int id);
+
+// Gets the first scan of an enrollment
+// Return:
+//	0 - ACK
+//	1 - Enroll Failed
+//	2 - Bad finger
+//	3 - ID in use
+int enroll1();
+
+// Gets the Second scan of an enrollment
+// Return:
+//	0 - ACK
+//	1 - Enroll Failed
+//	2 - Bad finger
+//	3 - ID in use
+int enroll2();
+
+// Gets the Third scan of an enrollment
+// Finishes Enrollment
+// Return:
+//	0 - ACK
+//	1 - Enroll Failed
+//	2 - Bad finger
+//	3 - ID in use
+int enroll3();
+
+// Checks to see if a finger is pressed on the FPS
+// Return: true if finger pressed, false if not
+int is_press_finger();
+
+// Deletes the specified ID (enrollment) from the database
+// Returns: true if successful, false if position invalid
+int delete_ID(int ID);
+
+// Deletes all IDs (enrollments) from the database
+// Returns: true if successful, false if db is empty
+int delete_all();
+
+// Checks the currently pressed finger against a specific ID
+// Parameter: 0-199 (id number to be checked)
+// Returns:
+//	0 - Verified OK (the correct finger)
+//	1 - Invalid Position
+//	2 - ID is not in use
+//	3 - Verified FALSE (not the correct finger)
+int verify1_1(int id);
+
+// Checks the currently pressed finger against all enrolled fingerprints
+// Returns:
+//	0-199: Verified against the specified ID (found, and here is the ID number)
+//	200: Failed to find the fingerprint in the database
+int identify1_N();
+
+// Captures the currently pressed finger into onboard ram
+// Parameter: true for high quality image(slower), false for low quality image (faster)
+// Generally, use high quality for enrollment, and low quality for verification/identification
+// Returns: True if ok, false if no finger pressed
+int capture_finger(int highquality);
+
+
+int CheckParsing(byte b, byte propervalue, byte alternatevalue, const char* varname);
+void Response_Packet(byte* buffer, struct response_packet* pack);
+word CalculateChecksumIn(byte* buffer, int length);
+word byte_to_word(byte* arr);
+void input_cmd(byte cmd, byte* arr );
+void SendCommand(byte cmd[], int length);
+struct response_packet* GetResponse();
+int IntFromParameter(byte*);
+
+#endif /* serial_com_h */
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <hidef.h>      /* common defines and macros */
 #include "derivative.h"      /* derivative-specific definitions */
 #include <mc9s12c32.h>
+#include "finger.h"
 
 
 
@@ -15,7 +16,8 @@ long int cycles =0;
 int LockFlag =1; // 1 if locked 0 if unlocked
 int testing1, LockDoor = 0;
 int prevTest1, prevLockButton = 0;
-
+int ACK = 1;
+int Error = 0x00;
 int pushbuttonsarefun = 0;
 //int fuckthisshit =0;
 
@@ -195,6 +197,462 @@ interrupt 7 void RTI_ISR(void)
       
       
 }
+
+byte GetHighByte(word w)
+{
+    return (byte)(w>>8)&0x00FF;
+}
+
+byte GetLowByte(word w)
+{
+    return (byte)w&0x00FF;
+}
+
+// returns the 12 bytes of the generated command packet
+// remember to call delete on the returned array
+word _CalculateChecksumOut(struct command_packet* pack)
+{
+    byte* Parameter = pack->Parameter;
+    byte* command = pack-> command;
+    word w = 0;
+    w += COMMAND_START_CODE_1;
+    w += COMMAND_START_CODE_2;
+    w += COMMAND_DEVICE_ID_1;
+    w += COMMAND_DEVICE_ID_2;
+    w += Parameter[0];
+    w += Parameter[1];
+    w += Parameter[2];
+    w += Parameter[3];
+    w += command[0];
+    w += command[1];
+    
+    return w;
+}
+
+
+word byte_to_word(byte* arr)
+{
+    word a = 0;
+    int i = 0;
+    for(i = 0; i< 2; i++) {
+        a += arr[i];
+        a = a<<8;
+    }
+    return a;
+}
+
+byte* GetPacketBytes(struct command_packet* pack)
+{
+    byte* Parameter = pack->Parameter;
+    byte* command = pack-> command;
+    static byte packetbytes[12];
+    
+    // update command before calculating checksum (important!)
+    word cmd = byte_to_word(command);
+    command[0] = GetLowByte(cmd);
+    command[1] = GetHighByte(cmd);
+    
+    word checksum = _CalculateChecksumOut(pack);
+    
+    packetbytes[0] = COMMAND_START_CODE_1;
+    packetbytes[1] = COMMAND_START_CODE_2;
+    packetbytes[2] = COMMAND_DEVICE_ID_1;
+    packetbytes[3] = COMMAND_DEVICE_ID_2;
+    packetbytes[4] = Parameter[0];
+    packetbytes[5] = Parameter[1];
+    packetbytes[6] = Parameter[2];
+    packetbytes[7] = Parameter[3];
+    packetbytes[8] = command[0];
+    packetbytes[9] = command[1];
+    packetbytes[10] = GetLowByte(checksum);
+    packetbytes[11] = GetHighByte(checksum);
+    
+    return packetbytes;
+}
+
+
+int CheckParsing(byte b, byte propervalue, byte alternatevalue, const char* varname)
+{
+    int retval = (b != propervalue) && (b != alternatevalue);
+    
+    return retval;
+}
+
+void Response_Packet(byte* buffer, struct response_packet* pack)
+{
+    byte* ParameterBytes = pack->ParameterBytes;
+    byte* ResponseBytes = pack->ResponseBytes;
+    byte* RawBytes = pack->RawBytes;
+    //CheckParsing(buffer[0], COMMAND_START_CODE_1, COMMAND_START_CODE_1, "COMMAND_START_CODE_1");
+    //CheckParsing(buffer[1], COMMAND_START_CODE_2, COMMAND_START_CODE_2, "COMMAND_START_CODE_2");
+    //CheckParsing(buffer[2], COMMAND_DEVICE_ID_1, COMMAND_DEVICE_ID_1, "COMMAND_DEVICE_ID_1");
+    //CheckParsing(buffer[3], COMMAND_DEVICE_ID_2, COMMAND_DEVICE_ID_2, "COMMAND_DEVICE_ID_2");
+    CheckParsing(buffer[8], 0x30, 0x31, "AckNak_LOW");
+    if (buffer[8] == 0x30) {
+        ACK = 1;
+    }
+    else
+        ACK = 0;
+    
+    CheckParsing(buffer[9], 0x00, 0x00, "AckNak_HIGH");
+    
+    word checksum = CalculateChecksumIn(buffer, 10);
+    byte checksum_low = GetLowByte(checksum);
+    byte checksum_high = GetHighByte(checksum);
+    CheckParsing(buffer[10], checksum_low, checksum_low, "Checksum_LOW");
+    CheckParsing(buffer[11], checksum_high, checksum_high, "Checksum_HIGH");
+    
+    //int Error = ParseFromBytes(buffer[5], buffer[4]);
+    
+    ParameterBytes[0] = buffer[4];
+    ParameterBytes[1] = buffer[5];
+    ParameterBytes[2] = buffer[6];
+    ParameterBytes[3] = buffer[7];
+    ResponseBytes[0]=buffer[8];
+    ResponseBytes[1]=buffer[9];
+    for (int i=0; i < 12; i++)
+    {
+        RawBytes[i]=buffer[i];
+    }
+}
+
+word CalculateChecksumIn(byte* buffer, int length)
+{
+    word checksum = 0;
+    for (int i=0; i<length; i++)
+    {
+        checksum +=buffer[i];
+    }
+    return checksum;
+}
+
+void input_cmd(byte cmd, byte* arr ) {
+    arr[0] = 0x00;
+    arr[1] = cmd;
+}
+
+void open() {
+    
+    struct command_packet cp;
+    input_cmd(Open, cp.command);
+    cp.Parameter[0] = 0x00;
+    cp.Parameter[1] = 0x00;
+    cp.Parameter[2] = 0x00;
+    cp.Parameter[3] = 0x00;
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    
+    return ;
+}
+
+void close()
+{
+    
+    struct command_packet cp;
+    input_cmd(Close, cp.command);
+    cp.Parameter[0] = 0x00;
+    cp.Parameter[1] = 0x00;
+    cp.Parameter[2] = 0x00;
+    cp.Parameter[3] = 0x00;
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    
+}
+
+void ParameterFromInt(int i, byte* Parameter)
+{
+    Parameter[0] = (i & 0x000000ff);
+    Parameter[1] = (i & 0x0000ff00) >> 8;
+    Parameter[2] = (i & 0x00ff0000) >> 16;
+    Parameter[3] = (i & 0xff000000) >> 24;
+}
+
+void SendCommand(byte cmd[], int length)
+{
+    
+}
+
+struct response_packet* GetResponse() {
+    static struct response_packet rp;
+    
+    
+    return &rp;
+}
+
+
+int is_press_finger()
+{
+    
+    struct command_packet cp;
+    input_cmd(IsPressFinger, cp.command);
+    //printf("%x%x", cp.command[0], cp.command[1]);
+    cp.Parameter[0] = 0x00;
+    cp.Parameter[1] = 0x00;
+    cp.Parameter[2] = 0x00;
+    cp.Parameter[3] = 0x00;
+    byte* packetbytes = GetPacketBytes(&cp);
+    //printf("packet bytes:\n");
+    //for (int i = 0; i < 12; i++) {
+    //	printf("Parameter[%d]: %x\n", i, packetbytes[i]);
+    //}
+    
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = 0;
+    int pval = rp->ParameterBytes[0];
+    pval += rp->ParameterBytes[1];
+    pval += rp->ParameterBytes[2];
+    pval += rp->ParameterBytes[3];
+    if (pval == 0)
+        retval = 1;
+    return retval;
+    return 0;
+}
+
+int ChangeBaudRate(unsigned long baud)
+{
+    if ((baud == 9600) || (baud == 19200) || (baud == 38400) || (baud == 57600) || (baud == 115200))
+    {
+        
+        
+        struct command_packet cp;
+        input_cmd(Close, cp.command);
+        ParameterFromInt(baud, cp.Parameter);
+        byte* packetbytes = GetPacketBytes(&cp);
+        ;
+        SendCommand(packetbytes, 12);
+        struct response_packet* rp = GetResponse();
+        int retval = ACK;
+        if (retval)
+        {
+            //_serial.end();
+            //_serial.begin(baud);
+        }
+        
+        return retval;
+    }
+    return 0;
+}
+
+int SetLED(int on)
+{
+    struct command_packet cp;
+    input_cmd(CmosLed, cp.command);
+    if (on)
+    {
+        cp.Parameter[0] = 0x01;
+    }
+    else
+    {
+        
+        cp.Parameter[0] = 0x00;
+    }
+    cp.Parameter[1] = 0x00;
+    cp.Parameter[2] = 0x00;
+    cp.Parameter[3] = 0x00;
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = 1;
+    if (ACK == 0)
+        retval = 0;
+    return retval;
+}
+
+int IntFromParameter(byte* ParameterBytes) {
+    int retval = 0;
+    retval = (retval << 8) + ParameterBytes[3];
+    retval = (retval << 8) + ParameterBytes[2];
+    retval = (retval << 8) + ParameterBytes[1];
+    retval = (retval << 8) + ParameterBytes[0];
+    return retval;
+}
+
+int get_enroll_count()
+{
+    struct command_packet cp;
+    input_cmd(GetEnrollCount, cp.command);
+    cp.Parameter[0] = 0x00;
+    cp.Parameter[1] = 0x00;
+    cp.Parameter[2] = 0x00;
+    cp.Parameter[3] = 0x00;
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    
+    int retval = IntFromParameter(rp->ParameterBytes);
+    
+    return retval;
+}
+
+int check_enrolled(int id)
+{
+    
+    struct command_packet cp;
+    input_cmd(CheckEnrolled, cp.command);
+    ParameterFromInt(id, cp.Parameter);
+    byte* packetbytes = GetPacketBytes(&cp);
+    
+    SendCommand(packetbytes, 12);
+    
+    struct response_packet* rp = GetResponse();
+    int retval = 0;
+    retval = ACK;
+    return retval;
+}
+
+int enroll_start(int id)
+{
+    struct command_packet cp;
+    input_cmd(EnrollStart, cp.command);
+    ParameterFromInt(id, cp.Parameter);
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = 0;
+    if (ACK == 0)
+    {
+        if (Error == NACK_DB_IS_FULL)
+            retval = 1;
+        if (Error == NACK_INVALID_POS)
+            retval = 2;
+        if (Error == NACK_IS_ALREADY_USED)
+            retval = 3;
+    }
+    
+    return retval;
+}
+
+int enroll1()
+{
+    struct command_packet cp;
+    input_cmd(Enroll1, cp.command);
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = IntFromParameter(rp->ParameterBytes);
+    if (retval < 20)
+        retval = 3;
+    else
+        retval = 0;
+    
+    if (ACK == 0)
+    {
+        if (Error == NACK_ENROLL_FAILED)
+            retval = 1;
+        if (Error == NACK_BAD_FINGER)
+            retval = 2;
+    }
+    
+    if (ACK == 1)
+        return 0;
+    else
+        return retval;
+}
+
+int enroll2()
+{
+    struct command_packet cp;
+    input_cmd(Enroll2, cp.command);
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = IntFromParameter(rp->ParameterBytes);
+    if (retval < 20)
+        retval = 3;
+    else
+        retval = 0;
+    
+    if (ACK == 0)
+    {
+        if (Error == NACK_ENROLL_FAILED)
+            retval = 1;
+        if (Error == NACK_BAD_FINGER)
+            retval = 2;
+    }
+    
+    if (ACK == 1)
+        return 0;
+    else
+        return retval;
+}
+
+int enroll3()
+{
+    struct command_packet cp;
+    input_cmd(Enroll3, cp.command);
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = IntFromParameter(rp->ParameterBytes);
+    if (retval < 20)
+        retval = 3;
+    else
+        retval = 0;
+    
+    if (ACK == 0)
+    {
+        if (Error == NACK_ENROLL_FAILED)
+            retval = 1;
+        if (Error == NACK_BAD_FINGER)
+            retval = 2;
+    }
+    
+    if (ACK == 1)
+        return 0;
+    else
+        return retval;
+}
+
+int identify1_N()
+{
+    struct command_packet cp;
+    input_cmd(Enroll3, cp.command);
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = IntFromParameter(rp->ParameterBytes);
+    if (retval > 20)
+        retval = 20;
+    return retval;
+}
+
+int capture_finger(int highquality)
+{
+    struct command_packet cp;
+    input_cmd(Enroll3, cp.command);
+    
+    if (highquality)
+    {
+        ParameterFromInt(1, cp.Parameter);
+    }
+    else
+    {
+        ParameterFromInt(0, cp.Parameter);
+    }
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = ACK;
+    return retval;
+    
+}
+
+int delete_ID(int id)
+{
+    struct command_packet cp;
+    input_cmd(DeleteID, cp.command);
+    ParameterFromInt(id, cp.Parameter);
+    byte* packetbytes = GetPacketBytes(&cp);
+    SendCommand(packetbytes, 12);
+    struct response_packet* rp = GetResponse();
+    int retval = ACK;
+    return retval;
+}
+
+
 
 
 
